d6a514bfce72c8bb2ee590afdeee2eaafe978de6
Gasidit <gasidit0anu@gmail.com>
2025-06-29 16:03:31 +0700

    mashala amigo thiss thing is stress af

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,11 +1,25 @@
 package com.example
 
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        module()
+    }.start(wait = true)
 }
 
 fun Application.module() {
-    configureRouting()
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
+    }
+
+    routing {
+        taskRoutes()
+    }
 }
